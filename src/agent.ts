48e6d863352f2244aeda31e57e18adb7a663cd31
drappier-charles <cdrappier@beamlit.com>
2025-04-03 16:53:47 +0200

    Update sdk version

--- src/agent.ts
+++ src/agent.ts
@@ -1,52 +1,33 @@
-import { getDefaultThread, wrapAgent } from "@blaxel/sdk";
+
+
+import { blModel, blTools } from "@blaxel/sdk";
 import { HumanMessage } from "@langchain/core/messages";
-import { CompiledGraph } from "@langchain/langgraph";
-import { FastifyRequest } from "fastify";
-import { v4 as uuidv4 } from "uuid";
-import { prompt } from "./prompt";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import fs from "fs";
 
-type InputType = {
-  inputs: string | null;
-  input: string | null;
-};
+interface Stream {
+  write: (data: string) => void;
+  end: () => void;
+}
 
-type AgentType = {
-  agent: CompiledGraph<any, any, any, any, any, any>;
-};
+export default async function agent(input: string, stream: Stream): Promise<void> {
+  const prompt = fs.readFileSync('./src/prompt.md', 'utf8');
+  const platformTools = await blTools(["exa", "gmail", "qdrant"]).ToLangChain();
+  const model = await blModel("gpt-4o-mini").ToLangChain();
+  const streamResponse = await createReactAgent({
+    llm: await model,
+    tools: [
+      ...await platformTools,
+    ],
+    prompt,
+  }).stream({
+    messages: [new HumanMessage(input)],
+  });
 
-const req = async (request: FastifyRequest, args: AgentType) => {
-  const { agent } = args;
-  const body = (await request.body) as InputType;
-  const thread_id = getDefaultThread(request) || uuidv4();
-  const input = body.inputs || body.input || "";
-  const responses: any[] = [];
-
-  const stream = await agent.stream(
-    { messages: [new HumanMessage(input)] },
-    { configurable: { thread_id } }
-  );
-
-  for await (const chunk of stream) {
-    responses.push(chunk);
+  for await (const chunk of streamResponse) {
+    if(chunk.agent) for(const message of chunk.agent.messages) {
+      stream.write(message.content)
+    }
   }
-  const content = responses[responses.length - 1];
-  return content.agent.messages[content.agent.messages.length - 1].content;
-};
-
-export const agent = wrapAgent(req, {
-  agent: {
-    metadata: {
-      name: "similar-company-finder",
-    },
-    spec: {
-      model: "gpt-4o-mini",
-      description: `
-      Find similar companies to the one provided
-      You'll be given a company name and you'll need to find similar companies
-      You can use the qdrant knowledge base to retrieve and store market analysis data
-      `,
-      prompt: prompt,
-    },
-  },
-  remoteFunctions: ["exa", "gmail", "qdrant"],
-});
+  stream.end();
+}
